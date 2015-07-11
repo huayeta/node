537406e4f858752e44eaba96e48bbf7e57894303
zh <zh@yingshangkeji.com>
2015-07-11 15:24:44 +0800

    add socket.io

---
+++ app.js
@@ -0,0 +1,71 @@
+var koa = require('koa');
+var http=require('http');
+var app = koa();
+
+require('./routers/index.js')(app);
+//静态资源文件目录
+var serve=require('koa-static');
+app.use(serve('./public'));
+
+var server=http.createServer(app.callback());
+
+var io=require('socket.io')(server);
+io.on('connection',function(socket){
+    socket.on('chat message',function(msg){
+        console.log('message:'+msg);
+        io.emit('new message',msg);
+    });
+    socket.on('disconnect',function(){
+        console.log('user disconnected!');
+    });
+})
+
+//var session=require('koa-session');
+
+//app.use(function *(next){
+//    this.body='header\n';
+//    yield saveResults.call(this);
+//    this.body+='footer\n';
+//});
+//
+//function *saveResults(){
+//    this.body+='results saved!\n';
+//}
+
+//app.keys=['1'];//加密秘钥
+//app.use(session(app));
+//
+//app.use(function *(next){
+//    var start=new Date();
+//    yield next;
+//    var ms=new Date()-start;
+//    this.set('X-Respose-Time',ms+'ms');
+//});
+//
+//app.use(function *(next){
+//    var start=new Date();
+//    yield next;
+//    var ms=new Date()-start;
+////    console.log('%s %s - %s',this.method,this.url,ms);
+//});
+//
+//app.use(function *(next){
+//    this.body='Hello World!';
+//    yield next;
+//});
+//
+//app.use(function *(next){
+////    console.log(this);
+//    this.body=this;
+//    this.cookies.set('name','zh01');
+////    console.log(this.request.ip);
+//    var n=this.session.views || 0;
+//    this.session.views=++n;
+//    this.body=n+' views';
+//});
+//
+//app.on('error',function(err,ctx){
+//    log.error('server',err,ctx);
+//})
+
+server.listen(3000);
--- routers/index.js
+++ routers/index.js
@@ -1,46 +1,99 @@
-var path=require('path');
-var Router=require('koa-router');
-var template=require('art-template');
-var accepts=require('accepts');
+module.exports = function(app){
+    var path=require('path');
+    var Router=require('koa-router');
+    var template=require('art-template');
+    var mongoose=require('mongoose');
+    var crypto=require('crypto');//加密用的
 
-var router=new Router();
+    var router=new Router();
 
-template.config('base',path.resolve(__dirname,'../views/'));
-template.config('extname','.htm');
+    template.config('base',path.resolve(__dirname,'../views/'));
+    template.config('extname','.htm');
 
-//模板的使用
-router.get('/',function *(next){
-//    this.body='hello world';
-    var DATA={title:'首页标题'};
-    this.body=template('index',DATA);
-});
+    //模板的使用
+    router.get('/',function *(next){
 
-//编写多个中间件
-router.get('/user/:id',function *(next){
-    yield next;
-    console.log(2);
-    this.body=this.params;
-},function *(next){
-    console.log(1);
-});
+        var DATA={title:'首页标题'};
+        this.body=template('index',DATA);
+    });
 
-//嵌套路由,预先设计好格式
-//*预设路径
-var tmpRouter=new Router({prefix:'/nest'});
-//*对参数预先处理
-tmpRouter.param('category',function *(category,next){
-//    console.log(category);//category的值
-    yield next;
-});
-//*对路径预先处理
-tmpRouter.use('/:category/:id',function *(next){
-//    console.log(this.params);
-    console.log(accepts(this.req).type());
-    yield next;
-})
-tmpRouter.get('/:category/:id',function *(next){
-    this.body=this.params;
-});
-router.get('/nest/:category/:id',tmpRouter.routes());
+    //数据库的操作
+    router.get('/mongoose',function *(next){
+        //连接到huayeta这个数据库
+        var db=mongoose.createConnection('127.0.0.1','huayeta');
 
-module.exports = router;
+        db.once('open',function(){
+            console.log('mongodb working!');
+            var Schema=mongoose.Schema;
+            var userSchema=new Schema({
+                name:{type:String,default:''},
+                age:{type:Number,min:0,max:150},
+                creattime:{type:Date,default:Date.now}
+            });
+            //对name字段过滤
+            userSchema.path('name').set(function(name){
+                return name+'1';
+            });
+            //对保存方法过滤
+            userSchema.pre('save',function(next){
+                console.log('save fired!');
+                next();
+            });
+            //这里会连接到users这个表
+            var userModel=db.model('user',userSchema);
+            //添加数据
+    //        var userEntity=new userModel({name:'zh01',age:19});
+    //        userEntity.save();
+            //查询数据，只列出name和age
+    //        userModel.find({},'name age',function(err,person){
+    //            console.log(person);
+    //        });
+            userModel.findById('559f429e8f74749107588376',function(err,person){
+                console.log(person);
+            });
+        });
+        db.on('error',function(){
+            console.log('connection error!');
+        });
+    });
+
+    //md5加密
+    router.get('/md5',function *(next){
+        this.body='请带上加密参数';
+    });
+    router.get('/md5/:content',function *(next){
+        var content=this.params.content;
+        var md5=crypto.createHash('md5');
+        md5.update(content,'utf8');
+        var body=md5.digest('hex');
+        this.body=content+'的md5值为：'+body;
+    });
+
+    //编写多个中间件
+    router.get('/user/:id',function *(next){
+        yield next;
+        console.log(2);
+        this.body=this.params;
+    },function *(next){
+        console.log(1);
+    });
+
+    //嵌套路由,预先设计好格式
+    //*预设路径
+    var tmpRouter=new Router({prefix:'/nest'});
+    //*对参数预先处理
+    tmpRouter.param('category',function *(category,next){
+    //    console.log(category);//category的值
+        yield next;
+    });
+    //*对路径预先处理
+    tmpRouter.use('/:category/:id',function *(next){
+    //    console.log(this.params);
+        yield next;
+    })
+    tmpRouter.get('/:category/:id',function *(next){
+        this.body=this.params;
+    });
+    router.get('/nest/:category/:id',tmpRouter.routes());
+    app.use(router.routes());
+};
